5af580aca47655f40f6d2fd0971141c4e8fcc38f
Axe1gr7 <122043202@upq.edu.mx>
2026-05-12 19:44:12 -0600

    practica_1
    practica 1

---
+++ practica_1/logica.js
@@ -0,0 +1,6 @@
+        //variable
+        let nombre= prompt("cual es tu nombre:")
+        //impresion de la variable en la consola 
+        console.log("hola: " + nombre + "!!")
+        //document ayuda a verlodirectamente en el html 
+        document.write("<h1> hola:  "+ nombre +" !! </h1>")
---
+++ practica_1/servidor.js
@@ -0,0 +1,34 @@
+
+
+console.log("Hola mundo js desde el servidor")
+
+// operaciones
+
+//variable 
+let edad1 = 11
+//variable que se comporta como constante 
+const edad2 = 42
+
+//imprimimos el promedio de las edades 
+console.log("edad promedio")
+console.log((edad1+edad2)/2)
+
+//medir tiempo de un proceso 
+//aqui declaramos la variable y se inicialZa el tiempo 
+console .time('miproceso')
+
+//este ciclo es el que se tiene que contar
+    for(let i=0; i < 1000000; i++)
+    {
+
+    }
+//aqui se determina la culminacion del tiempo 
+console.timeEnd('miproceso')
+
+
+// objetos tipo tabla 
+let usuarios= [
+    {nombre: "axelgr", edad:21},
+    {nombre: "chabela", edad:20}
+]
+console.table(usuarios)
