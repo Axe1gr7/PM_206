9d14e9c212fdda9debff1fb25ebeba8f233118a0
Axe1gr7 <122043202@upq.edu.mx>
2026-06-09 18:58:58 -0600

    practica 6 parte 1 (en clase)
    practica 6 parte 1 lo que realizamos en clase

---
+++ practica_6/miapp2/App.js
@@ -0,0 +1,36 @@
+/* Zona 1 importacion de componentes y archivos */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import {Saludo} from './components/Saludo';
+import { Salu2 } from './components/salu2';
+
+
+/* Zona 2 main - hogar de los componentes  */
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>-------------imagen------------------</Text>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>---------componente simple---------------</Text>
+      <Text>HOLA MUNDO</Text> 
+      <Text>------componentes propios ---------</Text>
+      <Text>-----1 componente propio ---------</Text>
+      <Saludo></Saludo>
+      <Text>-----varios componentes propios -------</Text>
+      <Salu2></Salu2>
+      <Text>--- fin ---</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+/* Zona estilos y posicionamiento*/
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
